10ac23aee059fb9edb265ecac838cf3d3c986d66
discphy <dinophy@nate.com>
2023-07-28 08:41:25 +0900

    feat(add): MemberServiceV2 added

---
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,63 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 파라미터 연동
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection con = dataSource.getConnection();
+
+        try {
+            con.setAutoCommit(false);
+
+            bizLogic(con, fromId, toId, money);
+            con.commit();
+        } catch (Exception e) {
+            con.rollback();
+            throw new IllegalStateException(e);
+        } finally {
+            release(con);
+        }
+    }
+
+    private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(con, fromId);
+        Member toMember = memberRepository.findById(con, toId);
+
+        memberRepository.update(con, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(con, toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+    private void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true);
+                con.close();
+            } catch (Exception e) {
+                log.error("error", e);
+            }
+        }
+    }
+}
