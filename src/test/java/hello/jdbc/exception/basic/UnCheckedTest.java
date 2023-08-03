678923332cc0e948f8d7f241eeb05dee8a367e3e
discphy <dinophy@nate.com>
2023-08-04 08:42:08 +0900

    test(add): Exception added

---
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedTest.java
@@ -0,0 +1,50 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class UnCheckedTest {
+
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        Assertions.assertThatThrownBy(service::callThrow)
+                .isInstanceOf(MyUncheckedException.class);
+    }
+
+    static class MyUncheckedException extends RuntimeException {
+        public MyUncheckedException(String message) {
+            super(message);
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyUncheckedException e) {
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        public void callThrow() {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            throw new MyUncheckedException("ex");
+        }
+    }
+}
